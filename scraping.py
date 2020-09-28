3636fa99d1f28e07453ce6860098d77cfbbfa6e1
Dan-Bas <67299365+Dan-Bas@users.noreply.github.com>
2020-09-27 22:08:09 -0400

    Mission to Mars

--- scraping.py
+++ scraping.py
@@ -7,9 +7,10 @@ import datetime as dt
 
 def scrape_all():
     # Initiate headless driver for deployment
-    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+    browser = Browser("chrome", executable_path='C:/Users/danib/OneDrive/Documents/Data Analytics/Class Folder/Web_Scrapping/chromedriver.exe', headless=False)
 
     news_title, news_paragraph = mars_news(browser)
+    hemisphere_image_urls = hemisphere_data(browser)
 
     # Run all scraping functions and store results in dictionary
     data = {
@@ -17,17 +18,13 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "URL_and_title": hemisphere_image_urls
     }
 
     # Stop webdriver and return data
     browser.quit()
     return data
-
-
-# # Set the executable path and initialize the chrome browser in splinter
-# executable_path = {'executable_path': 'C:/Users/danib/OneDrive/Documents/Data Analytics/Class Folder/Web_Scrapping/chromedriver.exe'}
-# browser = Browser('chrome', **executable_path, headless=False)
 
 
 def mars_news(browser):
@@ -59,7 +56,6 @@ def mars_news(browser):
         return None, None
         
     return news_title, news_p
-
 
 
 # ### Featured Images
@@ -116,6 +112,127 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemisphere_data(browser):
+
+        # 1. Use browser to visit the URL 
+        url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+        browser.visit(url)
+
+
+        # 2. Create a list to hold the images and titles.
+        hemisphere_image_urls = []
+
+        # 3. Write code to retrieve the image urls and titles for each hemisphere.
+        # Image 1.1 - Click 
+        browser.find_by_css('a')
+        images_click = browser.find_by_css('a')[5]
+        images_click.click()
+
+        # Image 1.2 - Browser 
+        html = browser.html
+        hem_image = soup(html, 'html.parser')
+
+        # Image 1.3 - Image and Title 
+        hem_image_1 = hem_image.select_one('div.downloads ul li a').get("href")
+        # hem_image_1
+        title1 = hem_image.find('h2').get_text()
+        # title1
+        
+
+        # Dict 1.4
+        Dict_1 = {
+            "image": hem_image_1,
+            "title": title1
+        }
+        # Dict_1
+
+        # Back to main page 1.5
+        browser.back()
+
+        # Image 2.1 - Click 
+        browser.find_by_css('a')
+        images_click = browser.find_by_css('a')[7]
+        images_click.click()
+
+        # Image 2.2 - Browser 
+        html = browser.html
+        hem_image = soup(html, 'html.parser')
+
+        # Image 2.3 - Image and Title 
+        hem_image_2 = hem_image.select_one('div.downloads ul li a').get("href")
+        # hem_image_2
+        title2 = hem_image.find('h2').get_text()
+        # title2
+        # print(hem_image_2, title2)
+
+        # Dict 2.4
+        Dict_2 = {
+            "image": hem_image_2,
+            "title": title2
+        }
+        # Dict_2
+
+        # Back to main page 2.5
+        browser.back()
+
+        # Image 3.1 - Click 
+        browser.find_by_css('a')
+        images_click = browser.find_by_css('a')[9]
+        images_click.click()
+
+        # Image 3.2 - Browser 
+        html = browser.html
+        hem_image = soup(html, 'html.parser')
+
+        # Image 3.3 - Image and Title 
+        hem_image_3 = hem_image.select_one('div.downloads ul li a').get("href")
+        # hem_image_3
+        title3 = hem_image.find('h2').get_text()
+        # title3
+        # print(hem_image_3, title3)
+
+        # Dict 3.4
+        Dict_3 = {
+            "image": hem_image_3,
+            "title": title3
+        }
+        # Dict_3
+
+        # Back to main page 3.5
+        browser.back()
+
+        # Image 4.1 - Click 
+        browser.find_by_css('a')
+        images_click = browser.find_by_css('a')[11]
+        images_click.click()
+
+        # Image 4.2 - Browser 
+        html = browser.html
+        hem_image = soup(html, 'html.parser')
+
+        # Image 4.3 - Image and Title 
+        hem_image_4 = hem_image.select_one('div.downloads ul li a').get("href")
+        # hem_image_4
+        title4 = hem_image.find('h2').get_text()
+        # title4
+        # print(hem_image_4, title4)
+
+        # Dict 4.4
+        Dict_4 = {
+            "image": hem_image_4,
+            "title": title4
+        }
+        # Dict_4
+
+        # Back to main page 4.4
+        browser.back()
+
+        # List of Dict
+        hemisphere_image_urls = [Dict_1, Dict_2, Dict_3, Dict_4]
+        # console.log(hemisphere_image_urls)
+
+        return hemisphere_image_urls
 
 if __name__ == "__main__":
 
